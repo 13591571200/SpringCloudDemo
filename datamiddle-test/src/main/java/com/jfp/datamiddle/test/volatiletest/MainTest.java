7f6b2b668331b6cd0fd93a7ec1f448aed8cfce5c
13591571200 <jfpgoodluck@163.com>
2020-12-20 00:35:58 +0800

    aaa

---
+++ datamiddle-test/src/main/java/com/jfp/datamiddle/test/volatiletest/MainTest.java
@@ -0,0 +1,29 @@
+package com.jfp.datamiddle.test.volatiletest;
+
+public class MainTest {
+
+    private volatile static boolean bool = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Thread0());
+        thread.start();
+
+        Thread.sleep(2000);
+
+        bool = false;
+
+        System.out.println("main - 结束");
+    }
+
+    public static class Thread0 implements Runnable{
+
+        @Override
+        public void run() {
+            while (bool){
+                System.out.println("a");
+            }
+
+            System.out.println("Thread2 - 结束");
+        }
+    }
+}
